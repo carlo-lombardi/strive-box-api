217db1ed1ea9ffa4ca7742de9dde8cadb9c87807
Ubeyt Demir <190541075@firat.edu.tr>
2021-04-16 13:24:34 +0300

    adds remove & edit name files

--- src/services/files/index.js
+++ src/services/files/index.js
@@ -12,6 +12,18 @@ import multer from "multer";
 
 import { v4 as uniqid } from "uuid";
 
+import { checkSchema, validationResult } from "express-validator";
+
+const schema = {
+  oldName: {
+    isString: true,
+    errorMessage: "Old name should be string!",
+  },
+  newName: {
+    isString: true,
+    errorMessage: "New name should be string!",
+  },
+};
 const upload = multer();
 
 const route = Router();
@@ -78,13 +90,86 @@ route.post(
 
 route.get("/:name", async (req, res, next) => {
   try {
+    const { type = "stream" } = req.query;
+    const finalDestination = join(publicFolderDirectory, req.params.name);
+    const file = await fs.readFile(finalDestination);
+    const stream = await fs.createReadStream(finalDestination);
+    if (type === "buffer") {
+      res.attachment(req.params.name);
+      res.send(file);
+    } else {
+      res.attachment(req.params.name);
+      stream.pipe(res);
+    }
+  } catch (err) {
+    const error = new Error(err.message);
+    error.httpStatusCode = 500;
+    next(error);
+  }
+});
+
+route.put("/", checkSchema(schema), async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    } else {
+      const { oldName, newName } = req.body;
+
+      const finalDestination = join(publicFolderDirectory, oldName);
+
+      const buffer = await fs.readFile(finalDestination);
+
+      let filesInDB = await fs.readJSON(dbPath);
+
+      await fs.remove(finalDestination);
+
+      const oldObj = filesInDB.find((f) => f.name !== req.params.name);
+
+      filesInDB = filesInDB.filter((f) => f.name !== oldName);
+
+      const newDestination = join(publicFolderDirectory, newName);
+
+      await fs.writeFile(newDestination, buffer);
+
+      const previewLink = `${req.protocol}://${req.hostname}:${process.env.PORT}/${newName}`;
+
+      const downloadLink = `${req.protocol}://${req.hostname}:${process.env.PORT}/files/${newName}`;
+
+      const newObj = {
+        ...oldObj,
+        name: newName,
+        previewLink,
+        downloadLink,
+        updatedAt: new Date(),
+      };
+
+      filesInDB.push(newObj);
+
+      await fs.writeJSON(dbPath, filesInDB);
+
+      res.send(newObj);
+    }
+  } catch (err) {
+    const error = new Error(err.message);
+    error.httpStatusCode = 500;
+    next(error);
+  }
+});
+
+route.delete("/:name", async (req, res, next) => {
+  try {
     const finalDestination = join(publicFolderDirectory, req.params.name);
 
-    const file = await fs.readFile(finalDestination);
+    let filesInDB = await fs.readJSON(dbPath);
 
-    res.writeHead("Content-Disposition", "attachment;"); // you should download
+    await fs.remove(finalDestination);
 
-    res.send(file);
+    filesInDB = filesInDB.filter((f) => f.name !== req.params.name);
+
+    await fs.writeJSON(dbPath, filesInDB);
+
+    res.send("OK");
   } catch (err) {
     const error = new Error(err.message);
     error.httpStatusCode = 500;
